ac92ef7afa81b3d8c34a65dd4b8786d032e3f425
Andrew Reed <stabr02@moravian.edu>
2018-05-26 01:59:12 -0400

    AddTwoNumbers Progress
    made lists with multiple nodes work
    made addition work with lists of different sizes work
    
    TODO: make carrying numbers work
    sloppy code, lots of duplicate code.

--- src/AddTwoNumbers2/AddTwoNumbers.java
+++ src/AddTwoNumbers2/AddTwoNumbers.java
@@ -14,6 +14,53 @@ import ClassImplementations.ListNode;
  */
 public class AddTwoNumbers {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode(l1.val + l2.val);
+        ListNode returnList = new ListNode(0);
+        ListNode cursor = returnList;
+        int carry = 0;
+
+        while(l1 != null && l2 != null){
+            cursor.val = l1.val + l2.val + carry;
+
+            if(cursor.val > 9){
+                cursor.val -= 10;
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+
+            l1= l1.next;
+            l2 = l2.next;
+
+            if(l1 != null || l2 != null){
+                cursor.next = new ListNode(0);
+                cursor = cursor.next;
+            }
+        }
+
+        if(l1 == null){
+            while(l2 != null){
+                cursor.val = l2.val;
+                l2 = l2.next;
+
+                if (l2 != null) {
+                    cursor.next = new ListNode(0);
+                    cursor = cursor.next;
+                }
+            }
+        }
+
+        if(l2 == null){
+            while(l1 != null){
+                cursor.val = l1.val;
+                l1 = l1.next;
+
+                if (l1 != null) {
+                    cursor.next = new ListNode(0);
+                    cursor = cursor.next;
+                }
+            }
+        }
+
+        return returnList;
     }
 }
