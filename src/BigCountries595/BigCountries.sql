afe436dd1aa55f2ab51659058fde5696a358edc0
Andrew Reed <stabr02@moravian.edu>
2018-06-06 22:56:47 -0400

    Added to post problem comment/reflection

--- src/BigCountries595/BigCountries.sql
+++ src/BigCountries595/BigCountries.sql
@@ -2,4 +2,6 @@ select name, population, area
 from World
 where area > 3000000
 or population > 25000000
-#pretty simple ad hoc query I would write every day at work
+#pretty simple ad hoc query I would write every day at work. I didn't know,
+#however, that doing a union of two selects was faster than an or statement in
+#the where clause. I'll keep that in mind
