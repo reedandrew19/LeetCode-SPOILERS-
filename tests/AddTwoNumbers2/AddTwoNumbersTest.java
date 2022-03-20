5823c2de1e1f4003588bd026060156f88ad4b6ac
reedandrew19 <stabr02@moravian.edu>
2022-03-20 19:09:10 -0400

    Adding Config files

--- tests/AddTwoNumbers2/AddTwoNumbersTest.java
+++ tests/AddTwoNumbers2/AddTwoNumbersTest.java
@@ -2,9 +2,9 @@ package AddTwoNumbers2;
 
 import ClassImplementations.ListNode;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by Andrew on 5/24/2018.
